ba8656d4b8ce571ec159fe8323a8b25d34cb8e78
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-09-19 00:04:52 -0300

    ejercicios completados

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# clase23-08-22
+# clase20-09-22
 
-### Ejercicio práctico 1
-Para realizar un testeo una empresa nos pide que realicemos una implementación donde con una caja con unos pesos fijos realicemos diferentes extracciones hasta que el monto que se requiera sea mayor a lo que hay en la caja. Para llevar un registro se debe mostrar:
-  - El restante de caja
-  - El monto total extraido
-  - La cantidad de extracciones
-
-### Ejercicio práctico 2
-Crear un programa de sumatoria, la sumatoria de un numero es la suma de consecutiva de todos sus antecesores
+### Ejercicio práctico
+Un competidor de carreras quiere registrar sus mejores tiempos por vuelta. Para eso nos encarga un programa para cargar vueltas que nos diga
+- Cantidad de vueltas
+- Vuelta más rápida
+- Vuelta más lenta
+- Promedio de tiempo por vuelta
--- ejercicio.py
+++ ejercicio.py
@@ -1,17 +1,30 @@
-caja = 5000
-extraccion = 0
-total_extracciones= 0
-cantidad_extracciones = 0
+mas_rapida = 0
+mas_lenta = 0
+promedio_de_vel = 0
+tiempo_vuelta = 0
+cant_vueltas = 0
+acumulador_tiempos = 0
 
-while (caja > extraccion):
-  print("Ingrese el monto de la extraccion")
-  extraccion = int(input())
-  if(caja > extraccion):
-    caja -= extraccion
-    total_extracciones += extraccion
-    cantidad_extracciones += 1
-    extraccion = 0
+while(tiempo_vuelta >= 0):
+  print("Ingrese el tiempo de la vuelta")
+  tiempo_vuelta = float(input())
+  if(tiempo_vuelta < 0):
+    break
+  acumulador_tiempos += tiempo_vuelta
 
-print("La cantidad sobrante en caja es $" + str(caja))
-print("El monto total extraido es $" + str(total_extracciones))
-print("La cantidad de extracciones es " + str(cantidad_extracciones))
+  if (cant_vueltas == 0):
+    mas_lenta = tiempo_vuelta
+    mas_rapida = tiempo_vuelta
+  
+  if(cant_vueltas > 0):
+    if(tiempo_vuelta < mas_rapida):
+      tiempo_vuelta = mas_rapida
+    if(tiempo_vuelta > mas_lenta):
+      tiempo_vuelta = mas_lenta
+  
+  cant_vueltas += 1
+
+print("La cantidad de vueltas fueron " + str(cant_vueltas))
+print("La vuelta más rápida fue de " + str(mas_rapida) + " segundos")
+print("La vuelta más lenta fue de " + str(mas_lenta) + " segundos")
+print("El promedio de tiempo por vuelta es de " + str(acumulador_tiempos/cant_vueltas) + " segundos")
