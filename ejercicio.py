2ac9fbbca871ecec4e07a4082cf9ca309750a319
Gonzalo Pozzoli <gonzalopozzoli99@gmail.com>
2022-10-17 18:17:20 -0300

    Ejercicios de prueba agregados

--- ejercicio.py
+++ ejercicio.py
@@ -1,30 +1,52 @@
-mas_rapida = 0
-mas_lenta = 0
-promedio_de_vel = 0
-tiempo_vuelta = 0
-cant_vueltas = 0
-acumulador_tiempos = 0
+lista = ["1", 2, 3, 5, "camilo"]
 
-while(tiempo_vuelta >= 0):
-  print("Ingrese el tiempo de la vuelta")
-  tiempo_vuelta = float(input())
-  if(tiempo_vuelta < 0):
-    break
-  acumulador_tiempos += tiempo_vuelta
+print(lista[1])
 
-  if (cant_vueltas == 0):
-    mas_lenta = tiempo_vuelta
-    mas_rapida = tiempo_vuelta
+lista[1] = "dos"
+
+print(lista[1])
+
+lista2dimensiones = [["Jonh","Doe"], [1,2,3]]
+
+print(lista2dimensiones[0][1])
+
+lista2dimensiones[0][1] = "Smith"
+
+print(lista2dimensiones[0][1])
+
+lista.append(6)
+
+print(lista)
+
+elementoEliminado = lista.pop(1)
+
+print(lista)
+print(elementoEliminado)
+
+# INSERT
+
+lista.insert(3, "leonel")
+print(lista)
+
+# EXTEND
+lista2 = [7,"leonel"]
+
+lista.extend(lista2)
+
+print(lista)
+
+# SORT
+listaNom = ["gonzalo", "sol", "camilo", "hernan", "estú"]
+listaLet = ["b", "a", "f", "c", "h"]
+listaNum = [7,2,5,15,8,5,7]
+listaNum.sort()
+listaLet.sort()
+
+print(listaNum)
+print(listaLet)
+
+for i in range(len(lista)):
+  print(lista[i])
   
-  if(cant_vueltas > 0):
-    if(tiempo_vuelta < mas_rapida):
-      tiempo_vuelta = mas_rapida
-    if(tiempo_vuelta > mas_lenta):
-      tiempo_vuelta = mas_lenta
-  
-  cant_vueltas += 1
-
-print("La cantidad de vueltas fueron " + str(cant_vueltas))
-print("La vuelta más rápida fue de " + str(mas_rapida) + " segundos")
-print("La vuelta más lenta fue de " + str(mas_lenta) + " segundos")
-print("El promedio de tiempo por vuelta es de " + str(acumulador_tiempos/cant_vueltas) + " segundos")
+for elemento in lista:
+  print(elemento)
